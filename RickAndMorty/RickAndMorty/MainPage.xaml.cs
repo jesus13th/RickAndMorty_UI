7c22ffd401aa4bcc0f1d6f2f0ec0ab359c26f282
jesus13th <jesus13th@gmail.com>
2022-04-20 06:09:43 -0600

    Agregar archivos de proyecto.

---
+++ RickAndMorty/RickAndMorty/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RickAndMorty.Models;
+using RickAndMorty.Services;
+
+using Xamarin.Forms;
+
+namespace RickAndMorty {
+    public partial class MainPage : ContentPage {
+        public List<Result> Characters = new List<Result>();
+        private RickAndMortyService rickAndMortyService;
+        private bool canNavigate = true;
+
+        public MainPage() {
+            InitializeComponent();
+            rickAndMortyService = new RickAndMortyService();
+        }
+        protected override void OnAppearing() {
+            base.OnAppearing();
+            GetResults();
+        }
+        private async void GetResults() {
+            canNavigate = false;
+            var async_characters = await rickAndMortyService.GetCharacters();
+            characterList.ItemsSource = async_characters.results;
+            counterLbl.Text = $"Page {rickAndMortyService.counter}/{async_characters.info.pages}";
+
+            previousBtn.IsEnabled = rickAndMortyService.counter > 1;
+            nextBtn.IsEnabled = rickAndMortyService.counter < async_characters.info.pages;
+            canNavigate = true;
+        }
+
+        private void nextBtn_Clicked(object sender, EventArgs e) {
+            if (canNavigate) {
+                rickAndMortyService.counter++;
+                GetResults();
+            }
+        }
+
+        private void previousBtn_Clicked(object sender, EventArgs e) {
+            if (canNavigate) {
+                rickAndMortyService.counter--;
+                GetResults();
+            }
+        }
+    }
+}
---
+++ RickAndMorty/RickAndMorty/Services/RickAndMortyService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using RickAndMorty.Models;
+
+namespace RickAndMorty.Services {
+    internal class RickAndMortyService {
+        private static RickAndMortyService instance;
+        public static RickAndMortyService Instance {
+            get {
+                if (instance == null) {
+                    instance = new RickAndMortyService();
+                }
+                return instance;
+            }
+            private set { }
+        }
+        const string URL = "https://rickandmortyapi.com/api/character/?page=";
+        public uint counter = 1;
+        public async Task<Characters> GetCharacters() {
+            var client = new HttpClient();
+            var response = await client.GetAsync(URL+counter);
+            var content = await response.Content.ReadAsStringAsync();
+            var characters = JsonSerializer.Deserialize<Characters>(content);
+
+            return characters;
+        }
+    }
+}
